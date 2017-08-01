47d868898053e72bc3473279014ef30937e90aee
wevertonsreis <weverton.sant.reis@gmail.com>
2017-07-31 22:12:00 -0300

    finalizando terceira aula

--- android-sync/agenda/app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.java
+++ android-sync/agenda/app/src/main/java/br/com/alura/agenda/retrofit/RetrofitInicializador.java
@@ -1,6 +1,12 @@
 package br.com.alura.agenda.retrofit;
 
+import java.io.IOException;
+
 import br.com.alura.agenda.service.AlunoService;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
@@ -9,9 +15,16 @@ public class RetrofitInicializador {
     private Retrofit retrofit;
 
     public RetrofitInicializador() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
         retrofit = new Retrofit.Builder()
                 .baseUrl("http://192.168.0.192:8080/api/")
                 .addConverterFactory(JacksonConverterFactory.create())
+                .client(client)
                 .build();
     }
 
